e4418097f02a672df679041686c0d805a634ccf7
ysgrrzhc <huixingju1981@126.com>
2021-05-19 14:02:23 +0800

    pull

--- app/src/main/java/com/example/gener/rongyundemo/MainActivity.java
+++ app/src/main/java/com/example/gener/rongyundemo/MainActivity.java
@@ -1,22 +1,13 @@
 package com.example.gener.rongyundemo;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                startActivity(new Intent(MainActivity.this, ConversationListActivity.class));
-                finish();
-            }
-        }, 100);
     }
-
 }
