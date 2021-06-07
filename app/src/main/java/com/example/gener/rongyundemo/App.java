d02db9430b75b70f549f73e7fdfc4c5e1646a1b1
ysgrrzhc <huixingju1981@126.com>
2021-06-07 22:30:53 +0800

    pull

--- app/src/main/java/com/example/gener/rongyundemo/App.java
+++ app/src/main/java/com/example/gener/rongyundemo/App.java
@@ -5,33 +5,19 @@ import android.app.Application;
 import android.content.Context;
 import android.util.Log;
 
-import io.rong.imageloader.core.DisplayImageOptions;
-import io.rong.imageloader.core.display.FadeInBitmapDisplayer;
 import io.rong.imkit.RongIM;
 import io.rong.imlib.RongIMClient;
 import io.rong.push.RongPushClient;
 
 public class App extends Application {
-    private static DisplayImageOptions options;
-
     @Override
     public void onCreate() {
         super.onCreate();
         RongIM.init(this);
         initRongYun();
-        SealAppContext.init(this);
-        options = new DisplayImageOptions.Builder()
-                .showImageForEmptyUri(com.example.gener.rongyundemo.R.drawable.de_default_portrait)
-                .showImageOnFail(com.example.gener.rongyundemo.R.drawable.de_default_portrait)
-                .showImageOnLoading(com.example.gener.rongyundemo.R.drawable.de_default_portrait)
-                .displayer(new FadeInBitmapDisplayer(300))
-                .cacheInMemory(true)
-                .cacheOnDisk(true)
-                .build();
 
     }
-    private  String tokenTwo = "AvDzvgR9NsUd2/4q0rY0zs/hSsm8Khf4neAFy/0kkd5MN+ClwEjwIxnNS3oOOLvQzR/VaRjvT8/C6qy/tB9H9z2T5bm4jOkQ";
-    private  String tokenOne = "XVkW2sTA/aNVlvSFfaNU5s/hSsm8Khf4neAFy/0kkd5MN+ClwEjwI3r9zdarJ3dGPjuedmzXrVpmVYvBnsKFLYFwo9+SjawN";
+    private  String token = "AvDzvgR9NsUd2/4q0rY0zs/hSsm8Khf4neAFy/0kkd5MN+ClwEjwIxnNS3oOOLvQzR/VaRjvT8/C6qy/tB9H9z2T5bm4jOkQ";
 
     /**
      * 初始化融云
@@ -47,8 +33,7 @@ public class App extends Application {
 //            AppContext.init(this);
             // 连接融云服务器
 //            RongYUtils.connect(this);
-            connect(tokenTwo);
-//            connect(tokenOne);
+            connect(token);
         }
     }
 
@@ -67,10 +52,6 @@ public class App extends Application {
             }
         }
         return null;
-    }
-
-    public static DisplayImageOptions getOptions() {
-        return options;
     }
 
     private void connect(String token) {
